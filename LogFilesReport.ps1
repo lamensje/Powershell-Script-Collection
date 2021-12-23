52281fc039220ddac3a7cc261c9162da8fd55433
lamensje <43524674+lamensje@users.noreply.github.com>
2021-12-23 13:23:03 +0100

    Add files via upload

---
+++ LogFilesReport.ps1
@@ -0,0 +1,21 @@
+$Path = '\\fs1\Logs$'
+$Output = '\\fs1\Logs$\Inventarisatie\gebruikersoverzicht.html'
+
+Get-ChildItem $Path | Where-Object LastWriteTime -gt (Get-Date).AddDays(-7) | Where-Object name -Like "*.log" | ForEach-Object {Get-Content $_.VersionInfo.FileName -Tail 1} | Foreach-Object {
+	$Split = $_ -split '(?=\b )' 
+	
+	$Gebruiker = $Split | Select-Object -Index 1
+	$Computer = $Split | Select-Object -Last 2 | Select-Object -First 1
+	$Actie = $Split | Select-Object -First 1
+	$Tijd = ($Split | Select-Object -Last 3 | Select-Object -First 1) -split "," | Select-Object -First 1
+	$Datum = $Split | Select-Object -Last 4 | Select-Object -First 1
+
+	$specs =[pscustomobject]@{
+		'Gebruiker' = $Gebruiker
+		'PC Naam' = $Computer
+		'Actie' = $Actie
+		'Tijd' = $Tijd
+		'Datum' = $Datum
+	}
+	$specs
+} | Out-HtmlView -filepath $Output -PreventShowHTML -Title "Gebruikersoverzicht" -DisablePaging -PreContent {"<h1>Laatst gewijzigd: $(Get-Date)</h1>"}
