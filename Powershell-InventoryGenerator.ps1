e4fd2977b83d2a8f3dd1e8759f93da1193852200
lamensje <43524674+lamensje@users.noreply.github.com>
2022-01-05 16:49:29 +0100

    Seperated Office key and status, added date

--- Powershell-InventoryGenerator.ps1
+++ Powershell-InventoryGenerator.ps1
@@ -27,10 +27,10 @@ if ($Computerinfo.CsSystemSKUNumber.ToString() -eq "To Be Filled By O.E.M." -or 
 $BIOS = $Computerinfo.BiosSMBIOSBIOSVersion.ToString() + " (" + $Computerinfo.BiosFirmwareType.ToString() + ")" 
 
 # Windows version (Home / Pro for exmaple) and version (21H2 for exmaple)
-$OS = $Computerinfo.OsName.ToString() + " " + (Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name DisplayVersion | Select-Object -ExpandProperty DisplayVersion).ToString()
+$OS = $Computerinfo.OsName.ToString() + " " + (Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name DisplayVersion | Select-Object -ExpandProperty DisplayVersion).ToString() -replace "Microsoft Windows ","W" -replace "Insider Preview ",""
 
 # CPU name + count
-$CPU = Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name ProcessorNameString | Select-Object -ExpandProperty ProcessorNameString
+$CPU = (Get-ItemProperty Registry::HKEY_LOCAL_MACHINE\HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name ProcessorNameString | Select-Object -ExpandProperty ProcessorNameString) -replace "\s+ ",""
 if ($Computerinfo.CsNumberOfProcessors -ge 2) {$CPU += (" (x" + $Computerinfo.CsNumberOfProcessors.Tostring() + ")")}
 
 # RAM amount, config and speed
@@ -45,7 +45,7 @@ Get-WmiObject CIM_VideoController | Select-Object Name | ForEach-Object {$GPU +=
 
 # Disks name, Bus type (Sata / NVMe) and type (HDD / SSD)
 $Disks = $null
-Get-PhysicalDisk | Select FriendlyName, MediaType, BusType | ForEach-Object -Process {$Disks += $_.FriendlyName + " (" + $_.BusType + " " + $_.MediaType + ") + "} -End {$Disks = $Disks.TrimEnd(' + ')}
+Get-PhysicalDisk | Where-Object BusType -NotLike "USB" | Select FriendlyName, MediaType, BusType | ForEach-Object -Process {$Disks += $_.FriendlyName + " (" + $_.BusType + " " + $_.MediaType + ") + "} -End {$Disks = $Disks.TrimEnd(' + ')}
 
 # Boot (C:\) drive free space in GB
 $DiskFree = ([math]::Round((Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DeviceID -eq "C:" }| Select-Object -ExpandProperty FreeSpace) / 1gb)).Tostring() + "GB"
@@ -61,18 +61,18 @@ if ($Excel32) {$Excel = $Excel32; $ospp = $x32 + "\Office16\OSPP.VBS"}
 if ($Excel64) {$Excel = $Excel64; $ospp = $x64 + "\Office16\OSPP.VBS"}
 $DisplayVersion = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" -Name "DisplayVersion" -ErrorAction SilentlyContinue | Where-Object {$_.DisplayVersion -eq ($Excel.VersionInfo.ProductVersion | Select-Object -First 1) -and $_.PSChildName -notlike "{*}"}
 $OfficeVerision = $null
-Get-ItemProperty -Path $DisplayVersion.PSPath | ForEach-Object {$OfficeVerision += ($_.PSChildName + " v" + $_.DisplayVersion + $(if ($_.InstallLocation -eq $x32) {" 32 Bit"} else {" 64 Bit "}))}
+Get-ItemProperty -Path $DisplayVersion.PSPath | ForEach-Object {$OfficeVerision += ($_.PSChildName + " v" + $_.DisplayVersion + $(if ($_.InstallLocation -eq $x32) {" (32 Bit)"} else {" (64 Bit) "})); $OfficeVerision += ($_.Name + "+ ")} -End {$OfficeVerision = $OfficeVerision.TrimEnd(',+ ')}
 
 # Office Licence
-$OfficeLicenceStatus = (cscript $ospp /dstatus)
+$OfficeLicence = (cscript $ospp /dstatus)
 
-if (($OfficeLicenceStatus | Select-String "LICENSED") -ne $null) {
-	$OfficeParsed = (($OfficeLicenceStatus | Select-String "LICENSED" -Context 1) -split "`n" | Select-String -NotMatch "EXPIRATION") -split ": " | Select-String -NotMatch "STATUS", "key"
+if (($OfficeLicence | Select-String "LICENSED") -ne $null) {
+	$OfficeParsed = (($OfficeLicence | Select-String "LICENSED" -Context 1) -split "`n" | Select-String -NotMatch "EXPIRATION") -split ": " | Select-String -NotMatch "STATUS", "key"
 	$OfficeKey = $OfficeParsed[1].ToString()
-	$OfficeLicence = $OfficeParsed[0].ToString()
+	$OfficeLicenceStatus = ($OfficeParsed[0] -split "---")[1].ToString()
 } else {
-	$OfficeLicence = $null
-	$OfficeKey = ((($OfficeLicenceStatus | Select-String "Last*") -Split ":" | Select-String -NotMatch "Last*") | Select-Object -First 1).ToString() + ((($OfficeLicenceStatus | Select-String "STATUS*") -Split ":" | Select-String -NotMatch "STATUS*") | Select-Object -First 1).ToString()
+	$OfficeLicenceStatus = $null
+	$OfficeKey = ((($OfficeLicence | Select-String "Last*") -Split ":" | Select-String -NotMatch "Last*")[0].ToString() + (($OfficeLicence | Select-String "STATUS*") -Split ":" | Select-String -NotMatch "STATUS*")[0].ToString())
 }
 
 # Userprofile list, removing occasional domain name after username (in the most complicated way possible, probably)
@@ -95,8 +95,10 @@ $specs =[pscustomobject]@{
 	'Schijf Model' = $Disks
 	'Vrije ruimte opstartschijf' = $DiskFree
 	'Profielen op schijf' = $Profiles
-	'Office versie' = $OfficeVerision
-	'Office Licentie' = $OfficeKey + $OfficeLicence
+	'Office Versie' = $OfficeVerision
+	'Office Status' = $OfficeLicenceStatus
+	'Office Key' = $OfficeKey
+	'Laatste update' = $Date
 }	
 
 if ($ExistsDaily -eq $null){
